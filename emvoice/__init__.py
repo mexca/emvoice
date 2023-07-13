8ec92372119a94964a71a40358891838d2a82456
maltelueken <malte_lueken@arcor.de>
2023-07-13 15:07:39 +0200

    First commit

---
+++ emvoice/__init__.py
@@ -0,0 +1,9 @@
+"""Documentation about emvoice"""
+import logging
+
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+__author__ = "Malte Luken"
+__email__ = "m.luken@esciencecenter.nl"
+__version__ = "0.1.0"
